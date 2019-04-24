c3c3cdf3f169603a4f429b6180f1b6530fe28acc
rztprog <rztprog@outlook.fr>
2019-04-25 00:12:47 +0200

    add methods with addItem function & temporary values

--- js/todolist.js
+++ js/todolist.js
@@ -3,7 +3,31 @@
 ////////// VARIABLES /////////
 //////////////////////////////
 
+// A SUPPR
+const body = document.getElementsByTagName("body")[0];
+
+body.style.backgroundColor = "#222222";
+body.style.color = "#FFF";
 
 
 ////////// FUNCTION //////////
 //////////////////////////////
+
+
+Vue.component('todo-item', {
+	props: ['todo'],
+	template: '<li>{{ todo.text }}</li>'
+})
+
+var ulwrapper = new Vue({
+	el: ".ulwrapper",
+	data: {
+		toDoList: [
+		]
+	},
+	methods: {
+		addItem: function() {
+			return this.toDoList.push("{id: 0, text:'test' }");	
+		}
+	}
+})
