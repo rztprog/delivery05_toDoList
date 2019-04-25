a1ba86040bccbb3bdb6c3787ec8f6c89b8ed5c85
rztprog <rztprog@outlook.fr>
2019-04-25 16:51:15 +0200

    add clearList function

--- js/todolist.js
+++ js/todolist.js
@@ -12,13 +12,30 @@ var main = new Vue({
 	data: {
 		toDoList: [],
 		inputfield: "",
+		isDark: false,
 	},
 	methods: {
-
 		addItem: function() {
-			this.toDoList.push({ text: this.inputfield });
-			this.inputfield = "";
-		}
+			if(this.inputfield == "") {
+				return
+				// return redField();
+			}else{
+				for(let item of this.toDoList){
+					if(item == this.inputfield){
+						console.log("FOUND");
+					}
+				}
+				this.toDoList.push({ text: this.inputfield });
+				this.inputfield = "";
+			}
+		},
+		clearList: function() {
+			this.toDoList = [];
+		},
+		redField: function() {
+		
+		//	return setTimeout( () => (isDark) ? "rgba(170, 170, 170, 0.9)" : "rgba(230, 230, 230, 0.9)", 500);
+		},
 	}
 
 });
